9cb62c10c2cc895fa4106588ee6001ddff23a7d6
LukinAS <LukinAS@206D-2>
2024-03-04 14:18:51 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using Cars111;
+namespace CarRace
+{
+    public class Program
+    {
+        static void Main()
+        {
+            List<Car> cars = new List<Car>
+        {
+            new Car("E63 AMG", 100, 0),
+            new Car("RS 7", 110, 0),
+            new Car("M5 F10", 115, 0)
+        };
+
+            while (true)
+            {
+                Console.Clear();
+                foreach (var car in cars)
+                {
+                    car.Move();
+                    Console.WriteLine($"Car: {car.Name}, Speed: {car.CurrentSpeed}, Distance: {car.Distance}");
+                }
+
+                if (cars.TrueForAll(c => c.Distance >= 1000))
+                {
+                    Console.WriteLine("\nRace finished!");
+                    cars.Sort((x, y) => y.Distance.CompareTo(x.Distance));
+                    for (int i = 0; i < cars.Count; i++)
+                    {
+                        Console.WriteLine($"Position {i + 1}: {cars[i].Name}");
+                    }
+                    break;
+                }
+
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
